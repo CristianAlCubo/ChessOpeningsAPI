a97e17204cd93ceffe02b8182665841441e7289c
Cristian Cubillos <ccubillosreyes123@gmail.com>
2023-04-18 13:45:51 -0500

    Changed root text

--- start/routes.ts
+++ start/routes.ts
@@ -21,7 +21,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
-  return { hello: 'world' }
+  return { "Hola!": "Vista '/docs' para ver la documentación :D"}
 })
 
 Route.group(() => {
